7bbcb1f0aa3310c75c599c28a93c68801c718c2f
jdavids0 <jbrdavidson@gmail.com>
2022-05-05 12:05:06 -0600

    styling edits

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App d-flex flex-column align-items-center">
+    <div className="App container d-flex flex-column">
       <Form></Form>
     </div>
   );
